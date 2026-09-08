26d1aea8e75de19d2fd88f984a0f05aac910fd0c
AnibalRodriguezGT87 <anibal.rodriguez@4csonline.com>
2026-09-08 16:59:35 -0600

    added external properties to read credentials.

--- src/main/java/com/sftp/SftpService.java
+++ src/main/java/com/sftp/SftpService.java
@@ -1,21 +1,27 @@
 package com.sftp;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.sftp.session.DefaultSftpSessionFactory;
 
 @Configuration
 public class SftpService {
+    private final SftpProperties properties;
+
+    public SftpService(SftpProperties properties) {
+        this.properties = properties;
+    }
 
     @Bean
     public DefaultSftpSessionFactory sftpSessionFactory() {
         DefaultSftpSessionFactory factory =
                 new DefaultSftpSessionFactory(true);
 
-        factory.setHost("localhost");
-        factory.setPort(2222);
-        factory.setUser("test");
-        factory.setPassword("test");
+        factory.setHost(properties.getHost());
+        factory.setPort(properties.getPort());
+        factory.setUser(properties.getUsername());
+        factory.setPassword(properties.getPassword());
 
         factory.setAllowUnknownKeys(true);
         return factory;
